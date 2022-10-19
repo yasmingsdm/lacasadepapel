0365f986a6fd2c151af3f6918735ebbabab6b108
Yasmin Dal Medico <108807529+yasmingsdm@users.noreply.github.com>
2022-10-19 08:39:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ function calcular(){
     let numeroCidades = (document.querySelectorAll('input[type="checkbox"]:checked').length)
     let porcentagem =  (numeroCidades*100/15).toFixed(2)
     if (porcentagem ==100){
-        alert("Parabéns! Você conhece todas as cidades da série!")}
-        else if (porcentagem>50){
-    alert ("Uau! Você gosta muito de viajar! Você conhece " + porcentagem + "% das cidades da série! ")}
-    else{alert ("Você conhece " + porcentagem + "% das cidades da série! Que tal conhecer mais?")}
+        alert("Parabéns! Você conhece todas as cidades da série!")
+    }else if (porcentagem>50){
+        alert ("Uau! Você gosta muito de viajar! Você conhece " + porcentagem + "% das cidades da série! ")
+    }else{alert ("Você conhece " + porcentagem + "% das cidades da série! Que tal conhecer mais?")}
 
-// tirar os checks dos quadradinhos
-  document.getElementsById("rio").checked = false
-    }
+}
