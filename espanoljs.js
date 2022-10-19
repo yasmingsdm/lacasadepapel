2e8e8e1c56a82ec405074649e6de88a34c9c1b48
Yasmin Dal Medico <108807529+yasmingsdm@users.noreply.github.com>
2022-10-19 08:38:44 +0200

    Update espanoljs.js

--- espanoljs.js
+++ espanoljs.js
@@ -2,8 +2,8 @@ function calcular(){
     let numeroCidades = (document.querySelectorAll('input[type="checkbox"]:checked').length)
     let porcentagem =  (numeroCidades*100/15).toFixed(2)
     if (porcentagem ==100){
-        alert("¡Enhorabuena! ¡Conoces todas las ciudades!")}
-        else if (porcentagem>50){
-    alert ("¡Guau! ¡Te gusta mucho viajar! ¡Conoces " + porcentagem + "% de las ciudades! ")}
-    else{alert ("Conoces " + porcentagem + "% de las ciudades! ¿Qué te pareces visitar más ciudades con nosotros?")}
-        }
+        alert("¡Enhorabuena! ¡Conoces todas las ciudades!")
+    }else if (porcentagem>50){
+        alert ("¡Guau! ¡Te gusta mucho viajar! ¡Conoces " + porcentagem + "% de las ciudades! ")
+    }else{alert ("Conoces " + porcentagem + "% de las ciudades! ¿Qué te pareces visitar más ciudades con nosotros?")}
+}
